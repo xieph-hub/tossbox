a6bb1cb30e95f074290cf905eb49d4740f4ab29e
xieph-hub <vxiephraim@gmail.com>
2025-12-24 08:16:13 +0100

    Delete app directory

--- app/walletButton.js
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-
-export default function WalletButton() {
-  return <WalletMultiButton style={{ background: "#7c5cff", borderRadius: 12 }} />;
-}
