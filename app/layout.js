97b58a4392d12b7d4381193db580b5b988e22e5d
xieph-hub <vxiephraim@gmail.com>
2025-12-25 18:17:47 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,19 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
-import WalletContextProvider from '@/components/WalletProvider';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import WalletContextProvider from "@/components/WalletProvider";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'TossBox - Crypto Price Prediction Game',
-  description: 'Predict. Win. Repeat.',
+  title: "TossBox - Crypto Price Prediction Game",
+  description: "Predict. Win. Repeat.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <WalletContextProvider>
-          {children}
-        </WalletContextProvider>
+        <WalletContextProvider>{children}</WalletContextProvider>
       </body>
     </html>
   );
